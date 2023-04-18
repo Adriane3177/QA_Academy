30e51f3898d915512cb86625640780da4e8f5212
Adriane3177 <131054142+Adriane3177@users.noreply.github.com>
2023-04-18 12:19:24 -0300

    Refatoração Exercicio04_easy
    Refatoração Exercicio04_easy

--- Execucao.java
+++ Execucao.java
@@ -8,11 +8,15 @@ public class Execucao {
     //    System.out.println(exemploInstanciaDeClasse.nomeDaClasse);
     //    exemploInstanciaDeClasse.exibirNomeClasse();
         
-    String Primeirovalor = JOptionPane.showInputDialog("Digite o Primeiro Valor");
-    String Segundovalor = JOptionPane.showInputDialog("Digite o Segundo Valor");
-
-    Exercicio03_easy exercicio03_easy = new Exercicio03_easy();
-    System.out.println(exercicio03_easy.retornaValorTrocado(Primeirovalor,Segundovalor));
+    //    String Primeirovalor = JOptionPane.showInputDialog("Digite o Primeiro Valor");
+    //    String Segundovalor = JOptionPane.showInputDialog("Digite o Segundo Valor");
+    //    Exercicio03_easy exercicio03_easy = new Exercicio03_easy();
+    //    System.out.println(exercicio03_easy.retornaValorTrocado(Primeirovalor,Segundovalor));
     
+          String numerodigitadousuario = JOptionPane.showInputDialog("Digite o valor: ");
+          int numerodigitado = Integer.parseInt(numerodigitadousuario);
+          Exercicio04_easy exercicio04_easy = new Exercicio04_easy();
+          System.out.println(exercicio04_easy.retornaDobroValor(numerodigitado));
+ 
     }
 }
--- Exercicio04_easy.java
+++ Exercicio04_easy.java
@@ -3,13 +3,14 @@ import javax.swing.JOptionPane;
 public class Exercicio04_easy {
    
    
-    public static void main(String[] args) {
-        int numerodigitado, resultado;
-        String numerodigitadousuario = JOptionPane.showInputDialog("Digite o valor: ");
-        numerodigitado = Integer.parseInt(numerodigitadousuario);
+    public int retornaDobroValor(int numerodigitado) {
+        int resultado;
+        //String numerodigitadousuario = JOptionPane.showInputDialog("Digite o valor: ");
+        //numerodigitado = Integer.parseInt(numerodigitadousuario);
         resultado = numerodigitado * 2;
-        System.out.println("O dobro do número é: " + resultado);
-        JOptionPane.showMessageDialog(null, "O dobro do número é: " + resultado);
-   
+        //System.out.println("O dobro do número é: " + resultado);
+        //JOptionPane.showMessageDialog(null, "O dobro do número é: " + resultado);
+        return resultado;
+        
        }  
 }
